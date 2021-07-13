fea3c0cc2423fc397c5e3fd05a57471ee4bb57e4
Safiy Cham <csafiy@yahoo.com>
2021-07-12 19:14:17 -0700

    Part1b completed

--- part1/course_info/src/Total.js
+++ part1/course_info/src/Total.js
@@ -2,7 +2,7 @@ const Total = (props) => {
   return (
     <div>
       <p>
-        Number of exercises {props.totalExercises}
+        Number of exercises {props.parts[0]['exercises'] + props.parts[1]['exercises'] + props.parts[2]['exercises']}
       </p>
     </div>
   )
