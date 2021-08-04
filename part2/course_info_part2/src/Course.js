b9c4833a40d4108df7776a88d45c27404dc0d8da
Safiy Cham <csafiy@yahoo.com>
2021-08-04 09:09:32 -0700

    Completed Part2.1 course_info continues

---
+++ part2/course_info_part2/src/Course.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Part from './Part';
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </div>
+  );
+};
+
+export default Course;
