ceb89c54fefd33ecac4d03fd52d06f7e745be14e
Safiy Cham <csafiy@yahoo.com>
2021-08-05 11:15:12 -0700

    Completed Part2.3 - refactored Part2.2 with reducer func

--- part2/course_info_part2/src/Total.js
+++ part2/course_info_part2/src/Total.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
+  // const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
+
+  const sumReduced = course.parts.reduce((a, b) => {
+    return { exercises: a.exercises + b.exercises }
+  });  // returns an obj w/key exercises: sumtotal; 'return a.exercises + b.exercises' will return NaN bc 2nd iteration 'a.exercises' doesnt exist
+
+  const exercisesTotal = sumReduced.exercises;  // extract num val from obj
 
   return (
-    <p>Number of exercises {sum}</p>
+    <>
+      {/* <p>Number of exercises {sum}</p> */}
+      <p>Number of exercises reducer {exercisesTotal}</p>
+    </>
   );
 };
 
